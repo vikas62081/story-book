913e93fe340cb3e248e0fd45a1e9f2f9c3501185
Vikas Kumar Vishwakarma <vikas@vikass-MacBook-Air.local>
2022-04-21 07:04:31 +0530

    fixed testcases issue

--- src/DatePicker/DatePicker.tsx
+++ src/DatePicker/DatePicker.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DatePicker } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
+import './styles.css';
 // import './custom-theme.less';
 export const BasicDatePicker = ({ oneTap = true, block = true, ...rest }) => {
   return <DatePicker oneTap={oneTap} block={block} ranges={[]} {...rest} />;
